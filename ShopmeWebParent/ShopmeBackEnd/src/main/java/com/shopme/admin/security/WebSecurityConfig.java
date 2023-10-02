78d80e63a49b4abd119b54d5e4afa5cfb4de1fce
Le Minh Hien <leminhhien11112001>
2023-10-02 16:25:00 +0700

    Code user login page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,10 +23,22 @@ public class WebSecurityConfig{
      
 //		http.authorizeRequests().anyRequest().permitAll();
 		
-		http.authorizeHttpRequests(configurer -> configurer
-				.anyRequest().permitAll());
- 
+		 http.authorizeHttpRequests(configure -> configure
+	                .anyRequest().authenticated())
+	            .formLogin(form -> form
+	                .loginPage("/login")
+	                .usernameParameter("email")
+	                .permitAll()
+	            );
+	           
         return http.build();
     }
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+		
+	}
 
 }
