f2f1ac65663844ef6ae2f01bce479987c64cfa73
Le Minh Hien <leminhhien11112001>
2023-10-08 11:08:41 +0700

    Code listing Brands

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -0,0 +1,13 @@
+package com.shopme.admin.brand;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Brand;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>,
+								CrudRepository<Brand, Integer>{
+	
+	
+
+}
