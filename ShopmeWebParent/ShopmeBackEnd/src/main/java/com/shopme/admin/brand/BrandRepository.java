596a8b8b69a83a41c6c1fe74da5e6cd6679104dc
Le Minh Hien <leminhhien11112001>
2023-10-09 09:39:34 +0700

    Code Create, Update, Delete Brand Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -8,6 +8,6 @@ import com.shopme.common.entity.Brand;
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>,
 								CrudRepository<Brand, Integer>{
 	
-	
+	public Long countById(Integer id);
 
 }
