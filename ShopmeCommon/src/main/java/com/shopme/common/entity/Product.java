94acb7599c7e555d9ab616904620d97bda261c68
Le Minh Hien <leminhhien11112001>
2023-10-22 15:53:08 +0700

    Code ProductDetail Entity Class and test save Product with Details

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,7 +1,9 @@
 package com.shopme.common.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -71,6 +73,10 @@ public class Product {
 	
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private Set<ProductImage> images = new HashSet<>();
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+	private List<ProductDetail> details = new ArrayList<>();
+
 
 	public Integer getId() {
 		return id;
@@ -230,6 +236,18 @@ public class Product {
 
 	public void setImages(Set<ProductImage> images) {
 		this.images = images;
+	}
+	
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
+
+	public void addDetail(String name, String value) {
+		this.details.add(new ProductDetail(name, value, this));
 	}
 	
 	public void addExtraImage(String imageName) {
