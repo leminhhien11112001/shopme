37464c45973072c2bd694f3e0495201809eb200b
Le Minh Hien <leminhhien11112001>
2023-10-29 00:03:26 +0700

    FE_Code Customer Login and Logout

--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -2,9 +2,13 @@ package com.shopme.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +16,49 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig{
 	
 	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomerUserDetailsService();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+	}	
+	
+	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
      		
-		http.authorizeHttpRequests(configure -> configure.anyRequest().permitAll());
-        
-		return http.build();
+		 http.authorizeHttpRequests(configure -> configure
+				 	.requestMatchers("/customer").authenticated()
+				 	.anyRequest().permitAll()
+				 	
+	            )
+	            .formLogin(form -> form
+	                .loginPage("/login")
+	                .usernameParameter("email")
+	                .permitAll()
+	            )
+	            .logout(logout -> logout.permitAll())
+	            .rememberMe(remember -> remember
+	            				.key("1234567890_aBcDeFgHiJkLmNoPqRsTuVwXyZ")
+	            				.tokenValiditySeconds(14 * 24 * 60 * 60) //7 days
+	            		)
+	            .authenticationProvider(authenticationProvider());
+	           
+		 return http.build();
     }
+	
+
 	
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
