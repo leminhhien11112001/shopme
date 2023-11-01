ad46f99870b4e01e7481a59d357c46bf580e052a
Le Minh Hien <leminhhien11112001>
2023-11-01 18:43:06 +0700

    Refactor property of products and navigation

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -55,11 +55,6 @@ public class Product {
 
 	@Column(name = "discount_percent")
 	private float discountPercent;
-
-	private float length;
-	private float width;
-	private float height;
-	private float weight;
 	
 	@Column(name = "main_image", nullable = false)
 	private String mainImage;
@@ -184,38 +179,6 @@ public class Product {
 
 	public void setDiscountPercent(float discountPercent) {
 		this.discountPercent = discountPercent;
-	}
-
-	public float getLength() {
-		return length;
-	}
-
-	public void setLength(float length) {
-		this.length = length;
-	}
-
-	public float getWidth() {
-		return width;
-	}
-
-	public void setWidth(float width) {
-		this.width = width;
-	}
-
-	public float getHeight() {
-		return height;
-	}
-
-	public void setHeight(float height) {
-		this.height = height;
-	}
-
-	public float getWeight() {
-		return weight;
-	}
-
-	public void setWeight(float weight) {
-		this.weight = weight;
 	}
 
 	public Category getCategory() {
