e8ba50afd9a3016edcecf40c2fd4d39610e490b8
Le Minh Hien <leminhhien11112001>
2023-11-12 01:27:33 +0700

    FE_Code add product to shopping cart

---
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartRestController.java
@@ -0,0 +1,60 @@
+package com.shopme.shoppingcart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.common.entity.Customer;
+import com.shopme.common.exception.CustomerNotFoundException;
+import com.shopme.customer.CustomerService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class ShoppingCartRestController {
+	@Autowired 
+	private ShoppingCartService cartService;
+	
+	@Autowired 
+	private CustomerService customerService;
+
+	@PostMapping("/cart/add/{productId}/{quantity}")
+	public String addProductToCart(@PathVariable("productId") Integer productId,
+			@PathVariable("quantity") Integer quantity, HttpServletRequest request) {
+
+		try {
+			Customer customer = getAuthenticatedCustomer(request);
+			Integer updatedQuantity = cartService.addProduct(productId, quantity, customer);
+
+			return updatedQuantity + " item(s) of this product were added to your shopping cart.";
+		} catch (CustomerNotFoundException ex) {
+			return "You must login to add this product to cart.";
+		} catch (ShoppingCartException ex) {
+			return ex.getMessage();
+		}
+
+	}
+
+	private Customer getAuthenticatedCustomer(HttpServletRequest request) 
+			throws CustomerNotFoundException {
+		String email = getEmailOfAuthenticatedCustomer(request);
+		if (email == null) {
+			throw new CustomerNotFoundException("No authenticated customer");
+		}
+
+		return customerService.getCustomerByEmail(email);
+	}
+	
+	private String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
+		Object principal = request.getUserPrincipal();
+		
+		if (principal == null) return null;
+
+		String customerEmail = null;
+
+		customerEmail = request.getUserPrincipal().getName(); 
+		
+		return customerEmail;
+	}	
+}
