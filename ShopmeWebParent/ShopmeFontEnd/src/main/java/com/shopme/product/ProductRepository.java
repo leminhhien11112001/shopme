2896d219258a1f79910c1020c815bbd7f2e66dc2
Le Minh Hien <leminhhien11112001>
2023-11-13 00:45:29 +0700

    FE_Code update product quantity in shopping cart

--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/product/ProductRepository.java
@@ -3,11 +3,13 @@ package com.shopme.product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Product;
 
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+public interface ProductRepository extends
+	CrudRepository<Product, Integer>, PagingAndSortingRepository<Product, Integer> {
 
 	@Query("SELECT p FROM Product p WHERE p.enabled = true "
 			+ "AND (p.category.id = ?1)"
--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -8,14 +8,20 @@ import org.springframework.stereotype.Service;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Product;
+import com.shopme.product.ProductRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired
 	private CartItemRepository cartRepo;
+	
+	@Autowired 
+	private ProductRepository productRepo;
 
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) 
 			throws ShoppingCartException {
@@ -60,4 +66,11 @@ public class ShoppingCartService {
 		
 		return customerEmail;
 	}	
+	
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
+	}
 }
