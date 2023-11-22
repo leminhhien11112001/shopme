025fa47a4853c6fef7bf10d46231cd395bb982c1
Le Minh Hien <leminhhien11112001>
2023-11-22 13:50:53 +0700

    Done add product to form

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -50,6 +50,7 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 
 	htmlCode = `
 		<div class="border rounded p-1" id="${rowId}">
+			<input type="hidden" name="detailId" value="0" />
 			<input type="hidden" name="productId" value="${productId}" class="hiddenProductId" />
 			<div class="row">
 				<div class="col-1">
@@ -71,6 +72,7 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 					<td>Product Cost:</td>
 					<td>
 						<input type="text" required class="form-control m-1 cost-input"
+							name="productDetailCost"
 							rowNumber="${nextCount}" 
 							value="${productCost}" style="max-width: 140px"/>
 					</td>
@@ -79,6 +81,7 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 					<td>Quantity:</td>
 					<td>
 						<input type="number" step="1" min="1" max="5" class="form-control m-1 quantity-input"
+							name="quantity"
 							id="${quantityId}"
 							rowNumber="${nextCount}" 
 							value="1" style="max-width: 140px"/>
@@ -88,6 +91,7 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 					<td>Unit Price:</td>
 					<td>
 						<input type="text" required class="form-control m-1 price-input"
+							name="productPrice"
 							id="${priceId}"
 							rowNumber="${nextCount}" 
 							value="${productPrice}" style="max-width: 140px"/>
@@ -97,6 +101,7 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 					<td>Subtotal:</td>
 					<td>
 						<input type="text" readonly="readonly" class="form-control m-1 subtotal-output"
+							name="productSubtotal"
 							id="${subtotalId}" 
 							value="${productPrice}" style="max-width: 140px"/>
 					</td>
@@ -104,8 +109,8 @@ function generateProductCode(productId, productName, mainImagePath, productCost,
 				<tr>
 					<td>Shipping Cost:</td>
 					<td>
-						<input type="text" required class="form-control m-1 ship-input" 
-							value="${shippingCost}" style="max-width: 140px"/>
+						<input type="text" required class="form-control m-1 ship-input"
+							name="productShipCost" value="${shippingCost}" style="max-width: 140px"/>
 					</td>
 				</tr>											
 			</table>
