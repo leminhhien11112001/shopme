beb445411a99599668b9914f04ae149df9cd1fe9
Le Minh Hien <leminhhien11112001>
2023-12-27 15:36:23 +0700

    Code update product quantity in shopping cart

--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -9,11 +9,17 @@ import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Product;
 import com.shopme.common.exception.ShoppingCartException;
+import com.shopme.product.ProductRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired private CartItemRepository cartRepo;
+	@Autowired private ProductRepository productRepo;
+
 
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) 
 			throws ShoppingCartException {
@@ -45,5 +51,12 @@ public class ShoppingCartService {
 	
 	public List<CartItem> listCartItems(Customer customer) {
 		return cartRepo.findByCustomer(customer);
+	}
+	
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
 	}
 }
