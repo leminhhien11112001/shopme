479728acf7dea423e82519f6c4bc6d9ccd5b0da2
Le Minh Hien <leminhhien11112001>
2023-12-28 10:36:16 +0700

    Code repository layer for order module

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.shopme.admin.order;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Order;
+
+public interface OrderRepository extends PagingAndSortingRepository<Order, Integer>,
+			CrudRepository<Order, Integer>{
+	
+}
