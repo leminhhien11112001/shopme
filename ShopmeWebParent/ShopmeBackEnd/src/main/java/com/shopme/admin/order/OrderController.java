04eb669b9609eee6983b3367654f67ba0e126ee5
Le Minh Hien <leminhhien11112001>
2023-12-28 11:14:24 +0700

    Code view Order Details

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderController.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
 import com.shopme.admin.paging.PagingAndSortingParam;
 import com.shopme.admin.setting.SettingService;
+import com.shopme.common.entity.Order;
 import com.shopme.common.entity.Setting;
+import com.shopme.common.exception.OrderNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -45,4 +49,20 @@ public class OrderController {
 			request.setAttribute(setting.getKey(), setting.getValue());
 		}	
 	}	
+	
+	@GetMapping("/orders/detail/{id}")
+	public String viewOrderDetails(@PathVariable("id") Integer id, Model model, 
+			RedirectAttributes ra, HttpServletRequest request) {
+		try {
+			Order order = orderService.get(id);
+			loadCurrencySetting(request);			
+			model.addAttribute("order", order);
+
+			return "orders/order_details_modal";
+		} catch (OrderNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+			return defaultRedirectURL;
+		}
+
+	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderService.java
@@ -1,8 +1,12 @@
 package com.shopme.admin.order;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.common.entity.Order;
+import com.shopme.common.exception.OrderNotFoundException;
 
 
 @Service
@@ -13,5 +17,13 @@ public class OrderService {
 
 	public void listByPage(int pageNum, PagingAndSortingHelper helper) {		
 		helper.listEntities(pageNum, ORDERS_PER_PAGE, repo);
+	}
+	
+	public Order get(Integer id) throws OrderNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new OrderNotFoundException("Could not find any orders with ID " + id);
+		}
 	}
 }
