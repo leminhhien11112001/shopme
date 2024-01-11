2e06abebd8ee38ac86ad4e9a5bd8dcf33d24ab8b
Le Minh Hien <leminhhien11112001>
2024-01-11 10:22:46 +0700

    Fix Error Brand

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -5,9 +5,6 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
@@ -18,10 +15,6 @@ import jakarta.persistence.Transient;
 @Entity
 @Table(name = "brands")
 public class Brand extends IdBasedEntity{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-
 	@Column(nullable = false, length = 45, unique = true)
 	private String name;
 
