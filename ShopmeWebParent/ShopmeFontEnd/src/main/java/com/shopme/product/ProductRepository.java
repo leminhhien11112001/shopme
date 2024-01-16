1ed698eac264ec7bd65711c3c91183450ce8ee93
Le Minh Hien <leminhhien11112001>
2024-01-16 17:51:02 +0700

    Code Write Review function part 3

--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/product/ProductRepository.java
@@ -2,6 +2,7 @@ package com.shopme.product;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -22,4 +23,10 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
 			+ "MATCH(name, short_description, full_description, alias) AGAINST (?1)", 
 			nativeQuery = true)
 	public Page<Product> search(String keyword, Pageable pageable);
+		
+	@Query("UPDATE Product p SET p.averageRating = CAST(COALESCE((SELECT AVG(r.rating) FROM Review r WHERE r.product.id = ?1), 0) AS java.lang.Float),"
+	        + " p.reviewCount = (SELECT COUNT(r.id) FROM Review r WHERE r.product.id = ?1) "
+	        + " WHERE p.id = ?1")
+	@Modifying
+	public void updateReviewCountAndAverageRating(Integer productId);
 }
