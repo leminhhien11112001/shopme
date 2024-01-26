41d2fc2b453a8f3457e9647ad3fa3acf41bf2ec3
Le Minh Hien <leminhhien11112001>
2024-01-26 11:26:31 +0700

    Code Sales Report by Date - Part 4

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportRestController.java
@@ -1,5 +1,9 @@
 package com.shopme.admin.report;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +35,15 @@ public class ReportRestController {
 			default:
 				return masterOrderReportService.getReportDataLast7Days();
 		}
+	}
+	
+	@GetMapping("/reports/sales_by_date/{startDate}/{endDate}")
+	public List<ReportItem> getReportDataByDatePeriod(@PathVariable("startDate") String startDate,
+			@PathVariable("endDate") String endDate) throws ParseException {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date startTime = dateFormatter.parse(startDate);
+		Date endTime = dateFormatter.parse(endDate);
+
+		return masterOrderReportService.getReportDataByDateRange(startTime, endTime);
 	}
 }
